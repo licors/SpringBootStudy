5cbaf9701d9c6fe99fb331ce3c45c6ff6f5be77c
DESKTOP-UGTRB8L\Ahn <licors214@gmail.com>
2020-11-30 18:25:43 +0900

    테스트 webEnvironment 가 RAMDOM_PORT 일 때 TestRstTemplate 사용하여 테스트하는 방법 (내부 톰캣 사용 O)

--- webServer/src/test/java/me/step2/demo/sample/SampleControllerTest.java
+++ webServer/src/test/java/me/step2/demo/sample/SampleControllerTest.java
@@ -3,30 +3,32 @@ package me.step2.demo.sample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringRunner.class)
-// WebEnvironment 가 Mock 일 때 테스트 방법 (내장 톰캣 X)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+// WebEnvironment 가 RANDOM_PORT 일 때 테스트 방법 (내장 톰캣 O)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
+
+    // 테스트시 이 목 빈을 사용한다.
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     public void name() throws Exception {
-        mockMvc.perform(get("/name"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello ahm"))
-                .andDo(print());
+        when(mockSampleService.getName()).thenReturn("white");
+
+        String result = testRestTemplate.getForObject("/name", String.class);
+        assertThat(result).isEqualTo("hello white");
     }
 }
