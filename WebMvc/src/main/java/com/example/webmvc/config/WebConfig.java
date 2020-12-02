29afb3071f9d03f715f3f82f4a8bdbe470392818
DESKTOP-UGTRB8L\Ahn <licors214@gmail.com>
2020-12-02 21:07:55 +0900

    SpringWebMvc 10.CORS - WebConfig 중복 구현으로 인한 에러 수정

--- WebMvc/src/main/java/com/example/webmvc/config/WebConfig.java
+++ WebMvc/src/main/java/com/example/webmvc/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.webmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/m/**")
                 .addResourceLocations("classpath:/m/")
                 .setCachePeriod(20);
+    }
+
+    // CORS 전역 설정할 때 이렇게 구현한다.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:18080");
     }
 }
